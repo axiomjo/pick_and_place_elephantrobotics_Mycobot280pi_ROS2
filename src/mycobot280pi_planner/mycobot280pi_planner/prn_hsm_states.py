04069a3e9c051317ef9bb522a4adf69259bbee80
axiomjo <jojo.josephined@gmail.com>
2025-10-29 15:32:43 +0700

    lets clean my branches

--- src/mycobot280pi_planner/mycobot280pi_planner/prn_hsm_states.py
+++ src/mycobot280pi_planner/mycobot280pi_planner/prn_hsm_states.py
@@ -11,11 +11,21 @@ from enum import Enum, auto
 from mycobot280pi_interfaces.action import ProcessWorkspace, SimpleCommandsAction
 
 class State(Enum):
+    #auto() is used coz the assigned number doesn't matter, as long as they're unique to each other
     IDLE = auto()
-    PROCESSING = auto()
+    BUSY = auto()
     CANCELLING = auto()
     ERROR = auto()
     DONE = auto() 
+
+    # BUSY superstate's states
+    busy_BUILDING_PLAN = auto()
+    busy_GETTING_NEXT_COMMAND = auto()
+    busy_SENDING_COMMAND = auto()
+    busy_WAITING_FOR_RESULT = auto()
+    busy_WAITING_FOR_MOVE_COMPLETE = auto()    
+    
+    
     
 class PlannerHSM:
     def __init__(self,node):
@@ -24,197 +34,189 @@ class PlannerHSM:
         """
         self.node = node
         
+        # --- FSM State ---
         self.state = State.IDLE 
         self.current_goal_handle = None
-        self.current_processing_task = None
+        
+        # --- The "Conveyor Belt" ---
+        self.plan_queue = []
+        self.current_command = None
+        
+        # --- closed loop feedback ---
+        self.target_angles = None
         
     def is_busy(self):
         """Check if the HSM is available for a new goal."""
-        return self.state != State.IDLE
+        return self.state in [
+            State.busy_BUILDING_PLAN,
+            State.busy_GETTING_NEXT_COMMAND,
+            State.busy_SENDING_COMMAND,
+            State.busy_WAITING_FOR_RESULT,
+            State.busy_WAITING_FOR_MOVE_COMPLETE
+        ]
         
-    async def handle_new_goal(self, goal_handle):
+    def handle_new_goal(self, goal_handle):
         """ method for processing complex goal handle """
-        self.node.get_logger().info("HSM: Received new complex goal.")
         
         if self.is_busy():
             # This shouldn't happen if the node's goal_callback is correct
             self.node.get_logger().warn("HSM: Was busy, goal should have been rejected.")
             return
-        
-        self.state = State.PROCESSING
+            
+        self.node.get_logger().info("HSM: Received new complex goal.")
         self.current_goal_handle = goal_handle
-        self.current_processing_task = asyncio.current_task() 
-
-        try:
-            # Await the coroutine *directly*.
-            final_result = await self._processing_loop(goal_handle)
-            return final_result
-
-        except asyncio.CancelledError:
-            # Safety net for cancellations
-            self.node.get_logger().warn("HSM: Main goal handler was cancelled.")
-
-            self.state = State.IDLE
-            self.current_goal_handle = None
-            self.current_processing_task = None
-
-            # We must NOT let the CancelledError escape to rclpy
-            result = ProcessWorkspace.Result()
-            result.success = False
-            result.message = "Process cancelled."
-            return result
+        self.transition_to(State.busy_BUILDING_PLAN)
          
      
-        
-    async def handle_cancel_request(self):
+    def handle_cancel_request(self):
         """ method for cancelled complex goal handle """
         self.node.get_logger().info("HSM: Received CANCELATION req for complex goal.")
         
-        if self.state != State.PROCESSING or not self.current_processing_task:
-            self.node.get_logger().warn("HSM: No active task to cancel.")
-            return
+        if self.is_busy():
+            self.transition_to(State.CANCELLING)
+    
+    def on_joint_angles_feedback(self,msg):
+        """  
+        public callback method 
+        for seeing if target position is reached  
+        """
         
-        self.state = State.CANCELLING
-        self.current_processing_task.cancel()
+        if self.are_angles_close(msg.joint_angles, self.target_angles);
+            self.node.get_logger().info("HSM: Robot move complete.")
+            self.transition_to(State.busy_GETTING_NEXT_COMMAND)
+    
+    
+    
+    def build_plan_queue(self):
+        """ 
+        method for breaking down complex command to command primitives.
         
-        await self.node.cancel_current_simple_command()
-    
-    
-    
-    async def _processing_loop(self, goal_handle):
-        """ method for breaking down complex command to command primitives"""
-        feedback = ProcessWorkspace.Feedback()
+        input:
+        
+        output: array containing SimpleCommandsAction.goal
+        """
+        
+        self.node.get_logger().info("HSM: Generating PLAN  QUEUE")
+        self.plan_queue = []
+        request = self.current_goal_handle.request
         
         try:
-            objects = goal_handle.request.objects_to_move.objects
-            targets = goal_handle.request.objects_target_position.points
-            orientations = goal_handle.request.objects_target_orientation
-            
-            feedback.current_state = "Initializing to Home"
-            goal_handle.publish_feedback(feedback)
-            
+            objects = request.objects_to_move.objects
+            targets = request.objects_target_position.points
+            orientations = request.objects_target_orientation
+        
             home_goal = SimpleCommandsAction.Goal(
-                command_type="move_joints", joint_angles=[0.0] * 6, speed=100
+                command_type="move_joints", 
+                joint_angles=[0.0] * 6, 
+                speed=100
             )
-            await self.node.execute_simple_command(home_goal)
+            self.plan_queue.append(home_goal)
+            
+            RX_DOWN = 180.0
+            RY_DOWN = 0.0
+            
             
             for i, (obj, target, orient) in enumerate(zip(objects, targets, orientations)):
             
-                if self.state == State.CANCELLING:
-                    raise asyncio.CancelledError()
                 
-                self.node.get_logger().info(f"--- Processing Object {i+1} ---")
-                feedback.current_state = f"Object {i+1}: Approaching pick pose"
-                feedback.current_object = obj
-                goal_handle.publish_feedback(feedback)
-                
-                rgb_yellow_goal = SimpleCommandsAction.Goal(command_type="set_rgb", r=255, g=255, b=0)
-                await self.node.execute_simple_command(rgb_yellow_goal)
-                
-                RX_DOWN = 180.0
-                RY_DOWN = 0.0
-                
-                approach_goal = SimpleCommandsAction.Goal(
-                    command_type="move",
-                    coords=[obj.center_point.x, obj.center_point.y, 70.0, RX_DOWN, RY_DOWN, 0.0],
-                    speed=100
-                )
-                await self.node.execute_simple_command(approach_goal)
-                
-                feedback.current_state = f"Object {i+1}: Descending and picking"
-                goal_handle.publish_feedback(feedback)
-                
-                rgb_red_goal = SimpleCommandsAction.Goal(command_type="set_rgb", r=255, g=0, b=0)
-                await self.node.execute_simple_command(rgb_red_goal)
-                
-                v_strong_goal = SimpleCommandsAction.Goal(command_type="vacuum_strong")
-                await self.node.execute_simple_command(v_strong_goal)
-                
-                descend_goal = SimpleCommandsAction.Goal(
-                    command_type="move",
-                    coords=[obj.center_point.x, obj.center_point.y, 30.0, RX_DOWN, RY_DOWN, 0.0],
-                    speed=50
-                )
-                await self.node.execute_simple_command(descend_goal)
-                
-                feedback.current_state = f"Object {i+1}: Ascending to clearance"
-                goal_handle.publish_feedback(feedback)
-                await self.node.execute_simple_command(home_goal)
-                
-                feedback.current_state = f"Object {i+1}: Moving to place pose"
-                goal_handle.publish_feedback(feedback)
-                
-                rgb_green_goal = SimpleCommandsAction.Goal(command_type="set_rgb", r=0, g=255, b=0)
-                await self.node.execute_simple_command(rgb_green_goal)
-                
-                approach_place_goal = SimpleCommandsAction.Goal(
-                    command_type="move",
-                    coords=[target.x, target.y, 70.0, RX_DOWN, RY_DOWN, float(orient)],
-                    speed=100
-                )
-                await self.node.execute_simple_command(approach_place_goal)
-                
-                feedback.current_state = f"Object {i+1}: Descending and placing"
-                goal_handle.publish_feedback(feedback)
-                
-                descend_place_goal = SimpleCommandsAction.Goal(
-                    command_type="move",
-                    coords=[target.x, target.y, 30.0, RX_DOWN, RY_DOWN, float(orient)],
-                    speed=50
-                )
-                await self.node.execute_simple_command(descend_place_goal)
-                
-                v_off_goal = SimpleCommandsAction.Goal(command_type="vacuum_off")
-                await self.node.execute_simple_command(v_off_goal)
-                
-                lift_up_goal = SimpleCommandsAction.Goal(
-                    command_type="move",
-                    coords=[target.x, target.y, 70.0, RX_DOWN, RY_DOWN, float(orient)],
-                    speed=100
-                )
-                await self.node.execute_simple_command(lift_up_goal)
-                
-            self.node.get_logger().info("All objects processed successfully.")
-            self.state = State.DONE
-            goal_handle.succeed()
-            
-            result = ProcessWorkspace.Result()
-            result.success = True
-            result.message = "All objects moved."
-            return result
-            
-        except asyncio.CancelledError:
-            self.node.get_logger().warn("Processing loop was CANCELLED.")
-            feedback.current_state = "Cancelling and returning home"
-            goal_handle.publish_feedback(feedback)
-            
-            home_goal = SimpleCommandsAction.Goal(
-                command_type="move_joints", joint_angles=[0.0] * 6, speed=100
+    def _get_home_goal(self):
+        """Returns a single 'home' goal."""
+        return SimpleCommandsAction.Goal(
+                command_type="move_joints", 
+                joint_angles=[0.0] * 6, 
+                speed=100
             )
-            await self.node.execute_simple_command(home_goal)
             
-            goal_handle.canceled()
-            self.state = State.DONE
+    def _get_pick_sequence(self, point, orientation ):
+        """ Returns a list of all actions for pick """
+        x,y = point.x, point.y
+        rx, ry, rz = orientation
+        
+        above_pick_coords =[x, y, 70.0, rx, ry, rz]
+        pick_coords = [x, y, 30.0, rx, ry, rz]
+        
+        return [
+        
+            # 1. Approach
+            SimpleCommandsAction.Goal(
+                command_type="set_rgb", 
+                r=255,  
+                g=255, 
+                b=0
+            ),
             
-            result = ProcessWorkspace.Result()
-            result.success = False
-            result.message = "Process cancelled by user."
-            return result
+            SimpleCommandsAction.Goal(
+                command_type="move",
+                coords=above_pick_coords,
+                speed=100
+            ),
             
-        except Exception as e:
-            self.node.get_logger().error(f"HSM Error: {str(e)}")
-            self.state = State.ERROR
-            goal_handle.abort()
             
-            result = ProcessWorkspace.Result()
-            result.success = False
-            result.message = f"Error: {str(e)}"
-            return result
             
-        finally:
-            self.node.get_logger().info("HSM returning to IDLE.")
-            self.state = State.IDLE
-            self.current_goal_handle = None
-            self.current_processing_task = None
+            # 2. Set for pick
+            SimpleCommandsAction.Goal(
+                command_type="set_rgb", 
+                r=255, 
+                g=0, 
+                b=0
+            ),
             
-    
+            SimpleCommandsAction.Goal(
+                command_type="vacuum_strong"
+            ),
+                
+            # 3. Descend to pick
+            SimpleCommandsAction.Goal(
+                command_type="move",
+                coords=pick_coords,
+                speed=50
+            ),
+            
+            SimpleCommandsAction.Goal(
+                command_type="move",
+                coords=above_pick_coords,
+                speed=100
+            ),
+        ]
+        
+        def _get_place_sequence(self, point, orientation):
+        """
+        Returns a LIST of all primitive commands
+        required to perform a "place" operation.
+        """
+        x, y = point.x, point.y
+        rx, ry, rz = orientation
+        
+        approach_coords = [x, y, 70.0, rx, ry, rz]
+        place_coords = [x, y, 30.0, rx, ry, rz]
+        
+        return [
+            # 1. Approach
+            SimpleCommandsAction.Goal(
+                command_type="set_rgb", 
+                r=255,  
+                g=255, 
+                b=0
+            ),
+            
+            SimpleCommandsAction.Goal(
+                command_type="move",
+                coords=approach_coords,
+                speed=100
+            ),
+            
+            # 2. Descend to place
+            SimpleCommandsAction.Goal(
+                command_type="move",
+                coords=approach_coords,
+                speed=100
+            ),
+            
+            # 3. Release
+            SimpleCommandsAction.Goal(command_type="vacuum_off"),
+            
+            # 4. Lift up
+            SimpleCommandsAction.Goal(command_type="move", coords=approach_coords, speed=100)
+        ]
+            
+        
