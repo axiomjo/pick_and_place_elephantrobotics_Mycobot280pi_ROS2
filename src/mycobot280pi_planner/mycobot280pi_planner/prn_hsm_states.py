420413308c3da2c909c618b02db2cce4bc65081a
axiomjo <jojo.josephined@gmail.com>
2025-11-03 16:53:07 +0700

    ADDED HSM using state design pattern
    ah, trus, krn aku pake actions, jadinya "kudu" async.
    blom nyoba sih klo yg biasa jadinya gmn.
    
    trus, ini events(transisi) nya blom kegambar btw.
    
    dan isinya hsm blom lengkap. lanjut di hp aja ya.

--- src/mycobot280pi_planner/mycobot280pi_planner/prn_hsm_states.py
+++ src/mycobot280pi_planner/mycobot280pi_planner/prn_hsm_states.py
@@ -10,213 +10,324 @@ import asyncio
 from enum import Enum, auto
 from mycobot280pi_interfaces.action import ProcessWorkspace, SimpleCommandsAction
 
-class State(Enum):
+# state is the fsm circles
+class StateID(Enum):
     #auto() is used coz the assigned number doesn't matter, as long as they're unique to each other
     IDLE = auto()
     BUSY = auto()
-    CANCELLING = auto()
+    CANCELLED = auto()
     ERROR = auto()
     DONE = auto() 
 
     # BUSY superstate's states
-    busy_BUILDING_PLAN = auto()
-    busy_GETTING_NEXT_COMMAND = auto()
-    busy_SENDING_COMMAND = auto()
-    busy_WAITING_FOR_RESULT = auto()
-    busy_WAITING_FOR_MOVE_COMPLETE = auto()    
+    busy_PLANNING = auto()
+    busy_EXECUTING = auto()
+    busy_RECORDING = auto()
     
+    # BUSY EXECUTING's states
+    busy_executing_FETCH_COMMAND = auto()
+    busy_executing_SEND_COMMAND = auto()
+    busy_executing_WAIT = auto()
+    busy_executing_WAIT_MOVE_COMPLETE = auto() 
+    busy_executing_READY = auto() 
     
+    # BUSY RECORDING's state
+    busy_recording_START = auto()
+    busy_recording_RECORD = auto()
+    busy_recording_END = auto()
+    busy_recording_SAVE = auto() 
+    busy_recording_DELETE = auto()
+
+
+# event is fsm arrow labels      
+class EventID(Enum):        
+    # --- Triggers from the outside world (PlannerNode) ---
+    NEW_GOAL = auto()        # User sent a new goal
+    CANCEL = auto()          # User requested a cancel
+    RESET = auto()           # User wants to reset from an ERROR/DONE state
     
+    # --- Triggers from hardware/monitors ---
+    ERROR_OCCURRED = auto()  # e.g., an emergency stop or motor fault
+    
+    # --- Internal events (states triggering other states) ---
+    PLANNING_COMPLETE = auto()
+    EXECUTION_COMPLETE = auto()
+    FETCH_NEXT_COMMAND = auto()
+    SEND_COMPLETE = auto()
+    MOVE_COMPLETE = auto()
+    RECORDING_COMPLETE = auto()
+    SAVE_COMPLETE = auto()
+    DELETE_COMPLETE = auto()
+     
+
 class PlannerHSM:
     def __init__(self,node):
         """
-        initializes the HSM. node id the ros2 planner node that will be injected here.
+        The 'Context' class that manages the states.
+        initializes the HSM. 
+        node is the ros2 planner node that will be injected here.
         """
         self.node = node
         
         # --- FSM State ---
-        self.state = State.IDLE 
+        self.current_state = None
+       
+        # --- ROS Action Properties ---
         self.current_goal_handle = None
         
         # --- The "Conveyor Belt" ---
         self.plan_queue = []
         self.current_command = None
         
-        # --- closed loop feedback ---
-        self.target_angles = None
+        # --- Async Synchronization to signal finished task ---
+        self.task_complete_event = None
+        self.final_result = None # Stores the action result
         
+        # --- closed loop feedback ---
+        self.target_coords = None
+        
+        # --- Start the machine asyncly ---
+        self.current_state = IdleState()
+        self.current_state.enter_sync(self)
+
+    async def transition_to(self, new_state):
+        """
+        Handles the async transition between states.
+        """
+        if self.current_state:
+            await self.current_state.exit(self)
+    
+        self.current_state = new_state
+        await self.current_state.enter(self)
+        
+        
+    async def handle_event(self, event: EventID, data=None):
+        """
+        The main internal async event handler.
+        """
+        self.node.get_logger().info(f"[HSM Event: {event.name}]")
+        new_state = await self.current_state.handle_event(self, event, data
+        
+        if new_state:
+            await self.transition_to(new_state)
+            
+    def get_current_state_id(self):
+        """Gets the enum ID of the most specific current state."""
+        return self.current_state.get_id()
+        
+    # --- Public API called by PlannerNode ---
     def is_busy(self):
-        """Check if the HSM is available for a new goal."""
-        return self.state in [
-            State.busy_BUILDING_PLAN,
-            State.busy_GETTING_NEXT_COMMAND,
-            State.busy_SENDING_COMMAND,
-            State.busy_WAITING_FOR_RESULT,
-            State.busy_WAITING_FOR_MOVE_COMPLETE
+        """
+        Called by goal_callback to check if the HSM is available.
+        """
+        current_id = self.get_current_state_id()
+        
+        is_resting = current_id in [
+            StateID.IDLE, 
+            StateID.DONE, 
+            StateID.CANCELLED, 
+            StateID.ERROR
         ]
         
-    def handle_new_goal(self, goal_handle):
-        """ method for processing complex goal handle """
+        return not is_resting
         
-        if self.is_busy():
-            # This shouldn't happen if the node's goal_callback is correct
-            self.node.get_logger().warn("HSM: Was busy, goal should have been rejected.")
-            return
-            
-        self.node.get_logger().info("HSM: Received new complex goal.")
+    async def handle_new_goal(self, goal_handle):
+        """
+        This is the main long-running task for the action server.
+        It will only return when the task is done, cancelled, or errors.
+        """
+        self.node.get_logger().info("HSM received new goal.")
         self.current_goal_handle = goal_handle
-        self.transition_to(State.busy_BUILDING_PLAN)
-         
+        self.final_result = None
+        self.task_complete_event = asyncio.Event()
+        
+        # (This will transition from IDLE to BUSY -> busy_PLANNING)
+        await self.handle_event(EventID.NEW_GOAL, goal_handle)
+        
+        await self.task_complete_event.wait()
+        
+        self.node.get_logger().info("HSM task finished. Returning result.")
+        return self.final_result
+        
+        
+    async def handle_cancel_request(self):
+        """
+        Called by the node's cancel_callback.
+        """
+        self.node.get_logger().warn("HSM processing CANCEL request.")
+        await self.handle_event(EventID.CANCEL)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+class State:
+    """
+    The base class 'interface' for all States.
+    async.
+    """
+    
+    def get_id(self):
+        """Returns the StateID enum for this state."""
+        # This forces all child classes to implement this method
+        raise NotImplementedError
+        
+    async def enter(self, hsm: PlannerHSM): 
+        """Called when this state is entered."""
+        # We can now use the ROS node for logging!
+        hsm.node.get_logger().info(f"Entering: {self.get_id().name}")
+        pass
+        
+    async def exit(self, hsm: PlannerHSM):
+        """Called when this state is exited."""
+        hsm.node.get_logger().info(f"Exiting: {self.get_id().name}")
+        pass
+        
+    def handle_event(self, robot, event, data=None):
+        """
+        Called to handle an event.
+        Must return a new State object if a transition is needed,
+        otherwise return None.
+        
+        'data' is an optional argument for passing payloads
+        (e.g., 'PICK_N_PLACE', {'x': 100, 'y': 50})
+        """
+        return None
+   
+# the top-level state classes
+"""
+    IDLE = auto()
+    BUSY = auto()
+    CANCELLED = auto()
+    ERROR = auto()
+    DONE = auto() 
+"""
+class DoneState(State):
+    def get_id(self):
+        return StateID.DONE
+        
+    async def enter(self, hsm: PlannerHSM):
+        await super().enter(hsm)
+        
+        result = ProcessWorkspace.Result()
+        result.success = True
+        result.message = "Task completed successfully."
+        hsm.final_result = result
+        
+        if hsm.current_goal_handle:
+            hsm.current_goal_handle.succeed()
+        
+        if hsm.task_complete_event:
+            hsm.task_complete_event.set()
+        
+class CancelledState(State):
+    def get_id(self):
+        return StateID.CANCELLED
+        
+    async def enter(self, hsm: PlannerHSM):
+        await super().enter(hsm)
+        
+        result = ProcessWorkspace.Result()
+        result.success = False
+        result.message = "Task was cancelled."
+        hsm.final_result = result
+        
+        if hsm.current_goal_handle:
+            hsm.current_goal_handle.canceled()
+            
+        if hsm.task_complete_event:
+            hsm.task_complete_event.set()
+
+class ErrorState(State):
+    def get_id(self):
+        return StateID.ERROR
+        
+    async def enter(self, hsm: PlannerHSM):
+        await super().enter(hsm)
+        
+        result = ProcessWorkspace.Result()
+        result.success = False
+        result.message = "Task failed with an error."
+        hsm.final_result = result
+        
+        if hsm.current_goal_handle:
+            hsm.current_goal_handle.abort()
+            
+        if hsm.task_complete_event:
+            hsm.task_complete_event.set()
+   
      
-    def handle_cancel_request(self):
-        """ method for cancelled complex goal handle """
-        self.node.get_logger().info("HSM: Received CANCELATION req for complex goal.")
+class BusyExecutingSendCommandState(State):
+    def get_id(self):
+        return StateID.busy_executing_SEND_COMMAND
         
-        if self.is_busy():
-            self.transition_to(State.CANCELLING)
-    
-    def on_joint_angles_feedback(self,msg):
-        """  
-        public callback method 
-        for seeing if target position is reached  
-        """
-        
-        if self.are_angles_close(msg.joint_angles, self.target_angles);
-            self.node.get_logger().info("HSM: Robot move complete.")
-            self.transition_to(State.busy_GETTING_NEXT_COMMAND)
-    
-    
-    
-    def build_plan_queue(self):
-        """ 
-        method for breaking down complex command to command primitives.
-        
-        input:
-        
-        output: array containing SimpleCommandsAction.goal
-        """
-        
-        self.node.get_logger().info("HSM: Generating PLAN  QUEUE")
-        self.plan_queue = []
-        request = self.current_goal_handle.request
+    async def enter(self, hsm: PlannerHSM):
+        await super().enter(hsm)
         
         try:
-            objects = request.objects_to_move.objects
-            targets = request.objects_target_position.points
-            orientations = request.objects_target_orientation
-        
-            home_goal = SimpleCommandsAction.Goal(
-                command_type="move_joints", 
-                joint_angles=[0.0] * 6, 
-                speed=100
-            )
-            self.plan_queue.append(home_goal)
+            # Assume a previous state (FETCH) set this
+            command_to_run = hsm.current_command
             
-            RX_DOWN = 180.0
-            RY_DOWN = 0.0
+            if not command_to_run:
+                raise Exception("No command was fetched.")
+     
+            hsm.node.get_logger().info("Calling execute_simple_command...")
+            await hsm.node.execute_simple_command(command_to_run)
             
+            await hsm.handle_event("SEND_COMPLETE")
             
-            for i, (obj, target, orient) in enumerate(zip(objects, targets, orientations)):
+        except asyncio.CancelledError:
+            hsm.node.get_logger().warn("execute_simple_command was cancelled.")
             
-                
-    def _get_home_goal(self):
-        """Returns a single 'home' goal."""
-        return SimpleCommandsAction.Goal(
-                command_type="move_joints", 
-                joint_angles=[0.0] * 6, 
-                speed=100
-            )
-            
-    def _get_pick_sequence(self, point, orientation ):
-        """ Returns a list of all actions for pick """
-        x,y = point.x, point.y
-        rx, ry, rz = orientation
-        
-        above_pick_coords =[x, y, 70.0, rx, ry, rz]
-        pick_coords = [x, y, 30.0, rx, ry, rz]
-        
-        return [
-        
-            # 1. Approach
-            SimpleCommandsAction.Goal(
-                command_type="set_rgb", 
-                r=255,  
-                g=255, 
-                b=0
-            ),
-            
-            SimpleCommandsAction.Goal(
-                command_type="move",
-                coords=above_pick_coords,
-                speed=100
-            ),
-            
-            
-            
-            # 2. Set for pick
-            SimpleCommandsAction.Goal(
-                command_type="set_rgb", 
-                r=255, 
-                g=0, 
-                b=0
-            ),
-            
-            SimpleCommandsAction.Goal(
-                command_type="vacuum_strong"
-            ),
-                
-            # 3. Descend to pick
-            SimpleCommandsAction.Goal(
-                command_type="move",
-                coords=pick_coords,
-                speed=50
-            ),
-            
-            SimpleCommandsAction.Goal(
-                command_type="move",
-                coords=above_pick_coords,
-                speed=100
-            ),
-        ]
-        
-        def _get_place_sequence(self, point, orientation):
-        """
-        Returns a LIST of all primitive commands
-        required to perform a "place" operation.
-        """
-        x, y = point.x, point.y
-        rx, ry, rz = orientation
-        
-        approach_coords = [x, y, 70.0, rx, ry, rz]
-        place_coords = [x, y, 30.0, rx, ry, rz]
-        
-        return [
-            # 1. Approach
-            SimpleCommandsAction.Goal(
-                command_type="set_rgb", 
-                r=255,  
-                g=255, 
-                b=0
-            ),
-            
-            SimpleCommandsAction.Goal(
-                command_type="move",
-                coords=approach_coords,
-                speed=100
-            ),
-            
-            # 2. Descend to place
-            SimpleCommandsAction.Goal(
-                command_type="move",
-                coords=approach_coords,
-                speed=100
-            ),
-            
-            # 3. Release
-            SimpleCommandsAction.Goal(command_type="vacuum_off"),
-            
-            # 4. Lift up
-            SimpleCommandsAction.Goal(command_type="move", coords=approach_coords, speed=100)
-        ]
-            
-        
+        except Exception as e:
+            hsm.node.get_logger().error(f"Failed to execute simple command: {e}")
+            await hsm.handle_event("ERROR")
+             
+    async def handle_event(self, hsm: PlannerHSM, event, data=None):
+        if event == "SEND_COMPLETE":
+            # Go to the next step in the execution engine
+            return BusyExecutingWaitMoveCompleteState() # (You'd define this class)
+        return None 
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
